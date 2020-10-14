417d7a5385fd3a2d2209cfc62bcb9ac76c8b1623
munificent-forum-1695 <bunchshawn@gmail.com>
2020-10-14 20:56:31 +0000

    Automatically backed up by Learn

---
+++ loop_while.rb
@@ -0,0 +1,16 @@
+n = 2
+count = 0
+while count <= n do
+  puts "I ran."
+  count += 1
+end
+
+3.times do
+  puts "Sherm ran."
+end
+
+counter = 0 
+until counter == 20
+  puts "The current number is #{counter + 1}."
+  counter += 1 
+end
